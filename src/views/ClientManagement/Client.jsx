2dc801379160d4fcace180bb029659f853863fa8
Kevin <Kevinchau328@gmail.com>
2024-04-19 19:04:18 -0400

    Image URI Compression

--- src/views/ClientManagement/Client.jsx
+++ src/views/ClientManagement/Client.jsx
@@ -4,14 +4,14 @@ import * as firebase from '../../../firebase/firebase';
 
 function Client() {
   const [title, setTitle] = useState('');
-  const [price, setPrice] = useState();
+  const [price, setPrice] = useState(0);
   const [description, setDescription] = useState('');
   const [imageUri, setImageUri] = useState('');
 
   useEffect(() => {
     // You can perform any additional actions here when imageUri changes,
     // such as uploading the image to Firebase storage.
-    console.log(imageUri);
+     console.log(imageUri);
   }, [imageUri]);
 
   const handleInputChange = (input, event) => {
@@ -27,15 +27,53 @@ function Client() {
   const handleImageUpload = (event) => {
     const file = event.target.files[0];
     const reader = new FileReader();
-
+  
     reader.onloadend = () => {
-      setImageUri(reader.result);
+      //Firebase Max Kb allowed for image URI is 1048487 bytes
+      compressImage(reader.result, 1048487, (compressedImage) => {
+        setImageUri(compressedImage);
+      });
     };
-
+  
     if (file) {
       reader.readAsDataURL(file);
     }
   };
+  
+  /*HTML5 CANVAS COMPRESSION*/
+  const compressImage = (dataUrl, maxSize, callback) => {
+    const image = new Image();
+    image.src = dataUrl;
+  
+    image.onload = () => {
+      const canvas = document.createElement('canvas');
+      const ctx = canvas.getContext('2d');
+  
+      let width = image.width;
+      let height = image.height;
+      let scaleFactor = 1;
+  
+      // Calculate the scaling factor to fit the image within the maxSize
+      if (image.size > maxSize) {
+        scaleFactor = Math.sqrt(maxSize / image.size);
+        width *= scaleFactor;
+        height *= scaleFactor;
+      }
+  
+      canvas.width = width;
+      canvas.height = height;
+  
+      // Draw the image onto the canvas with the new dimensions
+      ctx.drawImage(image, 0, 0, width, height);
+  
+      // Get the compressed data URL from the canvas
+      const compressedDataUrl = canvas.toDataURL('image/jpeg', 0.8);
+  
+      // Call the callback function with the compressed data URL
+      callback(compressedDataUrl);
+    };
+  };
+  
 
   const handleSubmit = () => {
     firebase.addANewMenuItem(title, price, description, imageUri);
